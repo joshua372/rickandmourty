59e1d8d4e1399c150c842daf68f16181dc7c42be
Josue Muñoz <joshua371@gmail.com>
2023-09-10 19:37:30 -0600

    Cambios ultimos prueba

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const pagina = 18
+const peticionCharacter = fetch(`https://rickandmortyapi.com/api/character/?page=${pagina}`)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const singlePeticion = 3
+const peticionLocation = fetch(`https://rickandmortyapi.com/api/location/${singlePeticion}`)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const episode = 28
+const peticionEpisode = fetch(`https://rickandmortyapi.com/api/episode/${episode}`)
+
+//Peticion para character
+peticionCharacter.then(resp => resp.json())
+.then( ( {results} ) =>{
+    // console.log(results)
+    // const reformattedArray = results.map(({ name, gender }) => {return '<table><tr><td>'+name+'</td><td>'+gender+'</td></tr></table>'})
+    // console.log(reformattedArray)
+    // let caja = document.createElement('p')
+    // caja.innerHTML(reformattedArray)
+    // document.body.append(caja)
+    let title = document.createElement("h1");
+    let dates = document.createElement("p");
+    const url = results[0].image
+    const img = document.createElement('img')
+    img.src = url
+    title.innerHTML= `Ejemplo del servicio character ${pagina}`
+    dates.innerHTML = `${results[0].name} - ${results[0].gender} `
+
+    document.body.append(title,img, dates)    
+       
+})
+.catch(console.warn)
+
+//Peticion para location
+
+peticionLocation.then(response => response.json())
+.then(data =>{
+console.log(data)
+let titleLocation = document.createElement("h1")
+let datesLocation = document.createElement("p")
+titleLocation.innerHTML=`Ejemplo del servicio location ${episode}`
+datesLocation.innerHTML = `${data.name} - ${data.type} `
+document.body.append(titleLocation, datesLocation)
+})
+.catch(console.warn)
+
+//Peticion para episode 
+
+peticionEpisode.then(response => response.json())
+.then(data =>{
+console.log(data)
+let titleEpisode = document.createElement("h1")
+let datesEpisode = document.createElement("p")
+titleEpisode.innerHTML=`Ejemplo del servicio Episode ${singlePeticion}`
+datesEpisode.innerHTML = `${data.name} - ${data.air_date} `
+document.body.append(titleEpisode, datesEpisode)
+})
+.catch(console.warn)
